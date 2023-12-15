b1107ef1ecdf0d2068adc828092e127012094cd4
Riddhi-shah19 <shriddhi19@gmail.com>
2023-12-16 01:31:30 +0530

    middlware and fuction for handling error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,17 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js'
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body
   // 10 is hash value
- const hashedPassword=bcryptjs.hashSync(password,10)
+  const hashedPassword = bcryptjs.hashSync(password, 10)
   // saving to db
-  const newUser = new User({ username, email, password:hashedPassword })
- try{
-  await newUser.save()
-  res.status(201).json('user created successfully')
- }catch(error){
-res.status(500).json(error.message)
- }
+  const newUser = new User({ username, email, password: hashedPassword })
+  try {
+    await newUser.save()
+    res.status(201).json('user created successfully')
+  } catch (error) {
+    next(error)
+  }
 }
--- api/index.js
+++ api/index.js
@@ -5,19 +5,29 @@ dotenv.config()
 // import userRouter from './routes/user.route'
 import authRouter from './routes/auth.route.js'
 
-const app=express()
+const app = express()
 
 app.use(express.json())
 
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose.connect(process.env.MONGO).then(() => {
     console.log('connected to mongodb')
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err)
 })
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('server is running on port 3000')
 })
 
 // app.use('/api/user',userRouter)
-app.use('/api/auth',authRouter)
+app.use('/api/auth', authRouter)
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'Internal server error'
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
