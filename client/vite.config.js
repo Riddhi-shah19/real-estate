664534bcc948215f1cd8dc7c981f0497a669d53f
Riddhi-shah19 <shriddhi19@gmail.com>
2023-12-22 00:06:51 +0530

    comlete signup functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      '/api':{
+        // for every /api requset add target at beginning 
+        target:'http://localhost:3000',
+        secure:false,
+      }
+    }
+  },
   plugins: [react()],
 })
