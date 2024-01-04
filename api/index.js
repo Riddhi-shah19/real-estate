8a4e9ed3b49e2f258c25b1855535515ce5ceba81
Riddhi-shah19 <shriddhi19@gmail.com>
2024-01-04 23:49:56 +0530

    user update api route

--- api/index.js
+++ api/index.js
@@ -1,13 +1,15 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-dotenv.config()
-// import userRouter from './routes/user.route'
+import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import cookieParser from 'cookie-parser'
+dotenv.config()
 
 const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('connected to mongodb')
@@ -19,8 +21,8 @@ app.listen(3000, () => {
     console.log('server is running on port 3000')
 })
 
-// app.use('/api/user',userRouter)
 app.use('/api/auth', authRouter)
+app.use('/api/user',userRouter)
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500
