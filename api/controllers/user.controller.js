bdbeb089afdc89ec05df26e2f5093224ac0e6dab
Riddhi-shah19 <shriddhi19@gmail.com>
2024-02-03 16:07:17 +0530

    create get user listing api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import Listing from "../models/listing.model.js"
 import User from "../models/user.model.js"
 import { errorHandler } from "../utils/error.js"
 import bcryptjs from 'bcryptjs'
@@ -28,18 +29,31 @@ export const deleteUser = async (req, res, next) => {
     if (req.user.id !== req.params.id) return next(errorHandler(401, 'You can only delete your own account'))
     try {
         await User.findByIdAndDelete(req.params.id)
-       res.clearCookie('access_token')
+        res.clearCookie('access_token')
         res.status(200).json('User has been deleted')
     } catch (error) {
         next(error)
     }
 }
 
-export const signOut=async (req,res,next)=>{
-    try{
+export const signOut = async (req, res, next) => {
+    try {
         res.clearCookie('access_token')
         res.status(200).json('User has been logged out')
-    }catch(error){
+    } catch (error) {
         next(error)
+    }
+}
+
+export const getUserListings = async (req, res, next) => {
+    if (req.user.id === req.params.id) {
+        try {
+            const listings = await Listing.find({ userRef: req.params.id })
+            res.status(200).json(listings)
+        } catch (error) {
+            next(error)
+        }
+    } else {
+        return next(errorHandler(401, 'you can only view your own listings'))
     }
 }
